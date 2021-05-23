7a4affbe0b21bc70d3dcd1f58686410bc3afdcfb
Petr Semiletov <tea@list.ru>
2021-05-23 19:25:27 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # lampa
 A Bash-script to control Elgato Key Light Air
+
+Deps: Bash, curl
+
+Before use:
+
+1. Connect your lamp with the router using Android/Iphone Control Center.
+
+2. Look at the router's settings pages for the lamp IP address.
+
+3. Edit the variable ADDR at this script for the actual IP of the lamp.
+
+
+When the lamp is turned on:
+
+1. Run lampa.sh
+
+2. Follow the on-screen help to control your lamp.
---
+++ lampa.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+ADDR='192.168.0.101'
+PORT='9123'
+
+#0..100
+BRIGHTNESS=50
+
+#2900..7000 - 143..344
+TEMPERATURE=200
+
+
+lamp_on() 
+{
+curl -s -X PUT http://$ADDR:$PORT/elgato/lights -H "Content-Type: application/json"  -d "{\"numberOfLights\": 1, \"lights\": [{ \"on\": 1}]}" > /dev/null
+}
+
+
+lamp_off() 
+{
+curl -s -X PUT http://$ADDR:$PORT/elgato/lights -H "Content-Type: application/json"  -d "{\"numberOfLights\": 1, \"lights\": [{\"on\": 0}]}" > /dev/null
+}
+
+
+lamp_brightness_set() 
+{
+# echo -n "brightness: " $BRIGHTNESS
+ curl -s -X PUT http://$ADDR:$PORT/elgato/lights -H "Content-Type: application/json"  -d "{\"numberOfLights\": 1, \"lights\": [{\"brightness\": $BRIGHTNESS}]}" > /dev/null
+}
+
+
+lamp_temperature_set() 
+{
+# echo -n "temperature: " $TEMPERATURE
+ curl -s -X PUT http://$ADDR:$PORT/elgato/lights -H "Content-Type: application/json"  -d "{\"numberOfLights\": 1, \"lights\": [{\"temperature\": $TEMPERATURE}]}" > /dev/null
+}
+
+
+greeting()
+{
+  echo 'q - quit\n'
+  echo 'z - on\n'
+  echo 'x - off\n'
+
+  echo '8 - brighter\n'
+  echo '2 - darker\n'
+
+}
+
+
+greeting
+
+while true; do
+read -rsn1 input
+
+
+if [ "$input" = "q" ]; then
+   exit 0;
+fi
+
+
+if [ "$input" = "z" ]; then
+   lamp_on
+fi
+
+
+
+if [ "$input" = "8" ]; then
+   if [ "$BRIGHTNESS" -lt 100 ]; then
+      ((BRIGHTNESS++))
+   fi
+   lamp_brightness_set
+fi
+
+
+if [ "$input" = "2" ]; then
+   if [ "$BRIGHTNESS" -gt 0 ]; then
+      ((BRIGHTNESS--))
+   fi
+   lamp_brightness_set 
+fi
+
+
+if [ "$input" = "4" ]; then
+   if [ "$TEMPERATURE" -gt 143 ]; then
+      ((TEMPERATURE--))
+   fi
+   lamp_temperature_set 
+fi
+
+
+if [ "$input" = "6" ]; then
+   if [ "$TEMPERATURE" -lt 344 ]; then
+      ((TEMPERATURE++))
+   fi
+
+   lamp_temperature_set 
+fi
+
+
+if [ "$input" = "x" ]; then
+   lamp_off
+fi
+
+
+done
